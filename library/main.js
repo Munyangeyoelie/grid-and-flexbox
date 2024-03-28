ead42034213b55c9de56a21319b54a673fa0cc58
Munyangeyoelie <munyangeyoelie38@gmail.com>
2024-03-28 17:38:08 +0300

    addtion of javascript

--- library/main.js
+++ library/main.js
@@ -1,4 +1,11 @@
-const myLibrary = [];
-const Book = () => {
-  this.name = "";
+var library = [];
+
+var Book = function Book(name, aothor, page) {
+  this.name = name;
+  this.aothor = aothor;
+  this.page = page;
+};
+
+Book.prototype.getInfo = function getInfo() {
+  return this.name + " by " + this.aothor + " on page " + this.page;
 };
