55afc4fd82adbfa1be89e8d779084c651469bff5
Munyangeyoelie <munyangeyoelie38@gmail.com>
2024-03-28 21:01:30 +0300

    js file

--- library/main.js
+++ library/main.js
@@ -1,11 +1,3 @@
-var library = [];
-
-var Book = function Book(name, aothor, page) {
-  this.name = name;
-  this.aothor = aothor;
-  this.page = page;
-};
-
-Book.prototype.getInfo = function getInfo() {
-  return this.name + " by " + this.aothor + " on page " + this.page;
-};
+const title = document.getElementById("title");
+const author = document.getElementById("author");
+const page = document.getElementById("page");
