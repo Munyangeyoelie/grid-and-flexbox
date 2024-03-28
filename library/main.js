cb2d351a0b856f8b9facfa78e9e8b34e3c96be59
Munyangeyoelie <munyangeyoelie38@gmail.com>
2024-03-28 22:57:26 +0300

    changes

--- library/main.js
+++ library/main.js
@@ -1,11 +1,11 @@
-const title = document.getElementById("title");
-const author = document.getElementById("author");
-const page = document.getElementById("page");
-const addBook = document.getElementById("addBook");
-const output = document.getElementById("output");
+// const title = document.getElementById("title");
+// const author = document.getElementById("author");
+// const page = document.getElementById("page");
+// const addBook = document.getElementById("addBook");
+// const output = document.getElementById("output");
 
-function myLibrary() {
-  output.innerHTML = title.value + author.value + page.value;
-}
-addBook.addEventListener("click", myLibrary);
-console.log("Hello");
+// function myLibrary() {
+//   output.innerHTML = title.value + author.value + page.value;
+// }
+// addBook.addEventListener("click", myLibrary);
+// console.log("Hello");
