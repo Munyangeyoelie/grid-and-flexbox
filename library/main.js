bfe5bbaa8de680ea3e8d06f0de55107f82c3ea3f
Munyangeyoelie <munyangeyoelie38@gmail.com>
2024-03-28 21:24:55 +0300

    changes in files

--- library/main.js
+++ library/main.js
@@ -1,3 +1,10 @@
 const title = document.getElementById("title");
 const author = document.getElementById("author");
 const page = document.getElementById("page");
+const addBook = document.getElementById("addBook");
+const output = document.getElementById("output");
+
+function myLibrary() {
+  output.innerHTML = title.value + author.value + page.value;
+}
+myLibrary();
