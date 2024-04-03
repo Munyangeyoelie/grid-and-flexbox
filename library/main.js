97562b8d74d0be34b684f72d2fa76c635acc3e29
Munyangeyoelie <munyangeyoelie38@gmail.com>
2024-04-03 22:16:26 +0300

    Adding funcyionality

--- library/main.js
+++ library/main.js
@@ -2,3 +2,4 @@ const newBook = document.getElementsByClassName(".new-book");
 console.log(newBook);
 const newForm = document.getElementById("ngeyo");
 console.log(newForm);
+const checkbox = document.getElementById("checkbox");
