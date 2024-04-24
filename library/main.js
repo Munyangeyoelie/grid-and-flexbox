31e4d89ea6a302b6a077980de2164cda22606b69
Munyangeyoelie <munyangeyoelie38@gmail.com>
2024-04-24 11:35:52 +0300

    changes

--- library/main.js
+++ library/main.js
@@ -1,16 +1,1 @@
-const openBtn = document.getElementsByClassName("add-new-book");
-const closeBtn = document.getElementById("close-btn");
-const modalCantainer = document.getElementsByClassName("new-book-form");
 
-//functionality
-openBtn.addEventListener("click", function () {
-  modalCantainer.style.display = "block";
-});
-closeBtn.addEventListener("click", function () {
-  modalCantainer.style.display = "none";
-});
-window.addEventListener("click", function (e) {
-  if (e.target == modalCantainer) {
-    modalCantainer.style.display = "none";
-  }
-});
