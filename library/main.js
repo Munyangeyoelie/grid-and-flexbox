656dd63abb57f735d906744ad8042d86cb418daf
Munyangeyoelie <munyangeyoelie38@gmail.com>
2024-04-24 13:47:13 +0300

    functionality

--- library/main.js
+++ library/main.js
@@ -10,9 +10,19 @@ function Book(title, author, pages, read) {
 
 function addBookToLibrary() {
   // do stuff here
+  let title = document.getElementById("title").value;
+  let author = document.getElementById("author").value;
+  let pages = document.getElementById("pages").value;
+  let read = document.getElementById("read").checked;
+  let newBook = new Book(title, author, pages, read);
+  //   myLibrary.push(newBook);
+  //   console.log(myLibrary);
 }
 let newBookBtn = document.querySelector("#new-book-btn");
 newBookBtn.addEventListener("click", function () {
   let newBookForm = document.querySelector("#new-book-form");
   newBookForm.style.display = "flex";
+});
+document.querySelector(".add-book").addEventListener("click", function () {
+  console.log("New Book");
 });
