521c4b876d20c7bdf45b37787ec55e58f62ecc16
Munyangeyoelie <munyangeyoelie38@gmail.com>
2024-04-24 11:29:58 +0300

    functionality

--- library/main.js
+++ library/main.js
@@ -1,5 +1,16 @@
-const newBook = document.getElementsByClassName(".new-book");
-console.log(newBook);
-const newForm = document.getElementById("ngeyo");
-console.log(newForm);
-const checkbox = document.getElementById("checkbox");
+const openBtn = document.getElementsByClassName("add-new-book");
+const closeBtn = document.getElementById("close-btn");
+const modalCantainer = document.getElementsByClassName("new-book-form");
+
+//functionality
+openBtn.addEventListener("click", function () {
+  modalCantainer.style.display = "block";
+});
+closeBtn.addEventListener("click", function () {
+  modalCantainer.style.display = "none";
+});
+window.addEventListener("click", function (e) {
+  if (e.target == modalCantainer) {
+    modalCantainer.style.display = "none";
+  }
+});
